33d0adaf2208bf4dae1e9c2fcb5e1959206dc439
DezmontDeXa <DezmontDeXa@DESKTOP-UMKOC9E>
2019-11-07 05:03:26 +0000

    Первая версия

---
+++ FreelancerProtectionLibTests/FreelancerProtectionTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FreelancerProtectionLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreelancerProtectionLib.Tests
+{
+	[TestClass()]
+	public class FreelancerProtectionTests
+	{
+		[TestMethod()]
+		public void CheckActivationTest_TimeExist()
+		{
+			FreelancerProtection.ActivationDate = DateTime.Now;
+			FreelancerProtection.Duration = new TimeSpan(0, 10, 0);
+
+			bool result = FreelancerProtection.CheckActivation();
+
+			if (result == false)
+				Assert.Fail("Ожидалось что время активации не истекло.");
+		}
+
+		[TestMethod()]
+		public void CheckActivationTest_TimeNotExist()
+		{
+			FreelancerProtection.ActivationDate = DateTime.Now - new TimeSpan(0, 20, 0);
+			FreelancerProtection.Duration = new TimeSpan(0, 10, 0);
+
+			bool result = FreelancerProtection.CheckActivation();
+
+			if (result == true)
+				Assert.Fail("Ожидалось что время активации истекло.");
+		}
+	}
+}
